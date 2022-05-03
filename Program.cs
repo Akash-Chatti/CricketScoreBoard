94230a3bd6a0cf0e663868e5b5341725c57cb663
Akash Chatti <31346414+Akash-Chatti@users.noreply.github.com>
2022-05-04 02:18:15 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using CricketScoreManagement.conductMatch;
+using CricketScoreManagement.teamComponents;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace CricketScoreManagement
+{
+    public class Program
+    {
+       // public static IMatch m;
+        //public Program(IMatch _m) => m = _m;
+        static void Main(string[] args)
+        {
+            var container = StartUp.ConfigureService();
+            var m = container.GetRequiredService<IMatch>();
+            m.configure();
+            m.playFirstInnings();
+            m.playSecondInnings();
+            m.result();
+        }
+
+       
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,25 @@
+﻿using CricketScoreManagement.conductMatch;
+using CricketScoreManagement.teamComponents;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CricketScoreManagement
+{
+    public class StartUp
+    {
+        public static IServiceProvider ConfigureService()
+        {
+            var services=new ServiceCollection()
+                
+                .AddTransient<IPlayer, Player>()
+                .AddTransient<ITeam, Team>()
+                .AddSingleton<IMatch, Match>()
+                .BuildServiceProvider();
+            return services;
+        }
+    }
+}
